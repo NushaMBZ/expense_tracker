a840aa70c8f6fdff4f0764a97c8696e6a2df0342
NushaMBZ <nushameenas253@gmail.com>
2024-09-11 14:35:00 +0530

    expense.dart Updated

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,10 +1,66 @@
+import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
+
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 141, 124, 236)
+);
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: Color.fromARGB(255, 4, 1, 48)
+);
 
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      home: const Expenses(),
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
+      theme: 
+        ThemeData().copyWith(
+          useMaterial3: true, 
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+          ),
+          cardTheme: CardTheme().copyWith(
+            color: kColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(
+              horizontal: 16,
+              vertical: 8),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer,
+            ),
+          ),
+          textTheme: ThemeData().textTheme.copyWith(
+            titleLarge: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: kColorScheme.onSecondaryContainer,
+              fontSize: 14,
+            ),
+          )
+        ),
+      themeMode: ThemeMode.system,  
+      home: const ExpenseTracker(),
     ),
   );
 }
+
+//scaffoldBackgroundColor: Color.fromARGB(255, 4, 1, 48)
